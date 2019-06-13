df81faffd16b0d92e167b4af593463f585107865
Bruno Logerfo <bclogerfo@hotmail.com>
2019-06-13 09:35:28 -0300

    code

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,72 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { Application/*, Context*/ } from 'probot' // eslint-disable-line no-unused-vars
+const yaml = require('js-yaml')
 
+const re = /fixes #(\d+)/ig
+
+//couldn't get this to work, i think `getLabel` is creating the label if it doesn't exist
+/*
+async function ensureLabelExists(context: Context, name: string) {
+    try {
+        return await context.github.issues.getLabel(context.repo({
+            name: name,
+        }))
+    } catch (e) {
+        return context.github.issues.createLabel(context.repo({
+            name: name,
+            color: '69D100'
+        }))
+    }
+}
+*/
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
-  })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+    app.on('pull_request.closed', async (context) => {
+        const pull = context.payload.pull_request
+        if (!pull.merged) {
+            return
+        }
+        const issues = new Set<string>()
+        let match = re.exec(pull.body)
+        while (match) {
+            issues.add(match[1])
+            match = re.exec(pull.body)
+        }
+        if (issues.size == 0) {
+            return
+        }
+        const content = await context.github.repos.getContents(context.repo({
+            path: '.github/close-label.yml',
+        }))
+        const config = yaml.safeLoad(Buffer.from(content.data.content, 'base64').toString())
+        issues.forEach(async id => {
+            const issue = await context.github.issues.get(context.issue({
+                number: id,
+            }))
+            const currentLabels = issue.data.labels.map(label => label.name)
+            const labels = new Set<string>()
+            currentLabels.forEach(label => {
+                if (labels.has(label)) {
+                    labels.delete(label)
+                }
+                else {
+                    const labelToAdd = config[label]
+                    if (labelToAdd) {
+                        labels.add(labelToAdd)
+                    }
+                }
+            })
+            if (labels.size == 0) {
+                return
+            }
+            const labelsToAdd = Array.from(labels)
+            /*
+            labelsToAdd.forEach(async label => {
+                await ensureLabelExists(context, label)
+            })
+            */
+            await context.github.issues.addLabels(context.issue({
+                labels: labelsToAdd,
+                number: id,
+            }))
+        })
+    })
 }
